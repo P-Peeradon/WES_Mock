086616e2065d137f02acb0959a0a8483f63bd4fb
P-Peeradon <p.determinant.integrate3@gmail.com>
2024-11-11 05:11:58 +0000

    generate transaction number

--- transaction.py
+++ transaction.py
@@ -1,6 +1,7 @@
 from WES_Mock.enrolment import Enrolment
 from WES_Mock.unit import Unit
 from WES_Mock.student import Student
+import random
 
 class Transaction:
     
@@ -12,10 +13,21 @@ class Transaction:
         self.__units: Unit = []
         self.__semester = semester
         self.__year = year
+        
+    def generate_trans_num(self) -> str:
+        trans_num = "U"
+        for i in range(13):
+            #since random.randint() return int, we have to cast str type.
+            trans_num += str(random.randint(0, 9))
+        
+        #This is called when submit the transaction and the transaction is completed.
+        return trans_num
+        
     
     def sum_credit(self):
         """
         This method will sum the credit in that semester
         """
         #Get unit from the enrolment data and sum all credit first
+        #Then sum credit of all unit in this transaction
         raise NotImplementedError("This method is not implemented")
