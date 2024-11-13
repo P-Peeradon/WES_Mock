6299d54136e061a10346663ef57102eb2fc34b35
P-Peeradon <p.determinant.integrate3@gmail.com>
2024-11-13 06:00:22 +0000

    Fixing word wrap in code and toString method returned String.

--- WES_Mock/student.py
+++ WES_Mock/student.py
@@ -17,7 +17,10 @@ class Student():
     #name of the table, if need to retrieve data from database.
     DB_TABLE_NAME = "student"
         
-    def __init__(self, id: str = "00000000", fname: str = "no name", lname: str = "no surname", date_of_birth: date = date(2000, 1, 1), program: str = "unknown", phone: str = "03XXXXXXXX", email: str = "XXX@hotmail.com", address: str = "unknown"):
+    def __init__(self, id: str = "00000000", fname: str = "no name" \
+            , lname: str = "no surname", date_of_birth: date = date(2000, 1, 1) \
+            , program: str = "unknown", phone: str = "03XXXXXXXX" \
+            , email: str = "XXX@hotmail.com", address: str = "unknown"):
         self.__id = id
         self.__fname = fname
         self.__lname = lname
@@ -144,7 +147,9 @@ class Student():
         Method to convert Student into String.
         return: String representation of Student (For testing the code).
         """
-        return f"id: {self.__id} \nname:{self.__fname + ' ' + self.__lname} \nDOB: {self.__date_of_birth.strftime('%-d %B %Y')}"
+        return f"id: {self.__id} \nname:{self.__fname + ' ' + self.__lname} \
+            \nDOB: {self.__date_of_birth.strftime('%-d %B %Y')} \nprogram: {self.__program} \n \
+            phone: {self.__phone} \nemail: {self.__email} \naddress: {self.__address}"
 
 
 
